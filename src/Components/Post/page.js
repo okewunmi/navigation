be56db06821e3a6f2209a95b0e833ce8110f5b35
okewunmi <okewunmiafeezolaide@gmail.com>
2024-05-27 07:20:33 +0100

    post

--- src/Components/Post/page.js
+++ src/Components/Post/page.js
@@ -28,6 +28,248 @@ const Post = () => {
             </div>
           </div>
         </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="head">
+            <h2>Title:</h2>
+            <p> Public Lecture</p>
+          </div>
+          <div className="head">
+            <h2>Details :</h2>
+            <p></p>
+          </div>
+          <div className="head">
+            <h2>venue :</h2>
+            <p></p>
+          </div>
+          <div className="time">
+            <div className="time-time">
+              <p>8:20pm</p>
+            </div>
+            <div className="time-date">
+              <p>02 May, 2024</p>
+            </div>
+          </div>
+        </div>
       </section>
     </Wrapper>
   );
